c9d65b74f2566ffcaad12d79ea281bb5cf5db5ec
augoff <michal.augoff@gmail.com>
2020-04-04 13:45:37 +0200

    Add reading model from cache if available

--- train.py
+++ train.py
@@ -1,20 +1,24 @@
+from pathlib import Path
+
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines import TRPO
+from stable_baselines import PPO2
 
 from environment import StudentEnv
 
-env = StudentEnv()
-
-model = TRPO(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=25000)
-model.save("a2c_cartpole")
-
-del model  # remove to demonstrate saving and loading
-
-model = TRPO.load("a2c_cartpole")
+env = StudentEnv(subjects_number=2)
+if Path('ppo2.zip').exists():
+    model = PPO2.load('ppo2')
+else:
+    model = PPO2(MlpPolicy, env, verbose=1)
+    model.learn(total_timesteps=25000)
+    model.save('ppo2')
 
 obs = env.reset()
 for i in range(2000):
-  action, _states = model.predict(obs)
-  obs, rewards, done, info = env.step(action)
-  env.render()
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    if done:
+        break
+    print(i)
+    env.render()
+
