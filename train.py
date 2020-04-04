5dc1741507520794b9ce150bfbbd34a8f5223e03
augoff <michal.augoff@gmail.com>
2020-04-05 01:44:28 +0200

    Add logs from the final model

--- train.py
+++ train.py
@@ -1,27 +1,38 @@
 from pathlib import Path
 
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines import PPO2
+from stable_baselines import PPO2, TRPO
+
 
 from environment import StudentEnv
 from reporting import setup_logging
 
+USE_RANDOM = False
 
 env = StudentEnv(subjects_number=2)
-if Path('ppo2_new.zip').exists():
-    model = PPO2.load('ppo2_new')
+# if Path('ppo2.zip').exists():
+#     model = PPO2.load('ppo2')
+if Path('ppo2_.zip').exists():
+    model = TRPO.load('ppo2_2048')
 else:
     model = PPO2(MlpPolicy, env, verbose=1, gamma=0.9)
     model.learn(total_timesteps=250000)
     model.save('ppo2_new')
 
-setup_logging('application.log')
+if USE_RANDOM:
+    setup_logging('application_random.log')
+else:
+    setup_logging('application_trpo.log')
 
-obs = env.reset()
-for i in range(20000):
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-    print(i)
-    env.render()
-    if done:
-        break
+for ep in range(200):
+    obs = env.reset()
+    for i in range(20000):
+        if USE_RANDOM:
+            action = env.action_space.sample()
+        else:
+            action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        print(i)
+        env.render()
+        if done:
+            break
